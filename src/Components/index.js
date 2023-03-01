7c6319daa6bee6b8ed1d1465959628d972fdf694
bora-sen <bahattinborasen@gmail.com>
2023-03-01 12:27:44 +0300

    +added components index

---
+++ src/Components/index.js
@@ -0,0 +1,7 @@
+export {default as Header} from './Header'
+export {default as Footer} from './Footer'
+export {default as Layout} from './Layout'
+export {default as Hero} from './Hero'
+export {default as Band} from './Band'
+export {default as Features} from './Features'
+export {default as LinkShorten} from './LinkShorten'
