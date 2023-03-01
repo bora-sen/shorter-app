d61b9cfb4275aac70866dabad5183f8c2d6728ed
bora-sen <bahattinborasen@gmail.com>
2023-03-01 12:24:50 +0300

    +added Assets

---
+++ src/Assets/index.js
@@ -0,0 +1,15 @@
+export {default as DetailedRecords} from './icon-detailed-records.svg'
+export {default as FullyCustomizableIcon} from './icon-fully-customizable.svg'
+
+export {default as FacebookIcon} from './icon-facebook.svg'
+export {default as InstagramIcon} from './icon-instagram.svg'
+export {default as PinterestIcon} from './icon-pinterest.svg'
+export {default as TwitterIcon} from './icon-twitter.svg'
+
+export {default as WorkingIllustration} from './illustration-working.svg'
+
+export {default as BrandLogo} from './logo.svg'
+export {default as BrandLogoWhite} from './logo-white.svg'
+export {default as BrandRecognition} from './icon-brand-recognition.svg'
+
+export {default as HamburgerIcon} from './icon-hamburger.svg'
