5b67c824a23975a4e6b1c6c3ab7dc7ef6fadc3dc
bora-sen <bahattinborasen@gmail.com>
2023-03-15 14:26:41 +0300

    +added Toaster element to Layout

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Footer from '../Footer'
 import Header from '../Header'
-//import Footer from '../Footer'
+import { Toaster } from 'react-hot-toast'
 
 function Layout({children}) {
   return (
     <main className='container mx-auto'>
+      <Toaster position='bottom center' />
       <Header />
         {children}
       <Footer />
